77c159485fcaf3791b6e41861a1bcfe6399f9db8
Isaacrgz <isaacrgz97@gmail.com>
2024-05-15 20:29:02 -0600

    method get the last friday

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Datetime
+
+This git has fast methods in java to work with Date, Time, Milliseconds, etc. For understanding, it's not simplified code, meaning, you can shorten the code.
--- src/main/java/datetime/MainApp.java
+++ src/main/java/datetime/MainApp.java
@@ -6,18 +6,22 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.temporal.TemporalAdjusters.previous;
 
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Date & Time working in Java");
-
         System.out.println("Format: day/mounth/year");
         System.out.println("Get date: " + getTodayDate());
         System.out.println("Get datetime: " + getTodayDateTime());
         System.out.println("Get millis from datetime: " + getDateTimeToMillis());
         System.out.println("Get datetime from millis: " + getMillisToDateTime(getDateTimeToMillis()));
-        System.out.println("Get millis from String: " + getDatetimeFromStringToMillis("14/05/2024") + ", reverse: " + getMillisToDateTime(getDatetimeFromStringToMillis("14/05/2024")));
-        System.out.println("Get millis from String: " + getDatetimeFromStringToMillis2("2024-05-14") + ", reverse: " + getMillisToDateTime(getDatetimeFromStringToMillis2("2024-05-14")));
+        System.out.println("Get millis from String: " + getDatetimeFromStringToMillis("14/05/2024") + ", reverse: "
+                + getMillisToDateTime(getDatetimeFromStringToMillis("14/05/2024")));
+        System.out.println("Get millis from String: " + getDatetimeFromStringToMillis2("2024-05-14") + ", reverse: "
+                + getMillisToDateTime(getDatetimeFromStringToMillis2("2024-05-14")));
+        System.out.println("Get last friday: " + getLastFriday());
     }
 
     public static String getTodayDate() {
@@ -60,6 +64,13 @@ public class MainApp {
     }
 
     public static Long getDatetimeFromStringToMillis2(String sDate) {
-        return LocalDateTime.parse(sDate + "T00:00:00").atZone(ZoneId.of("America/Mexico_City")).toInstant().toEpochMilli();
+        return LocalDateTime.parse(sDate + "T00:00:00").atZone(ZoneId.of("America/Mexico_City")).toInstant()
+                .toEpochMilli();
+    }
+
+    public static String getLastFriday() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate friday = LocalDate.now().with(previous(FRIDAY));
+        return friday.format(formatter);
     }
 }
